65067a3d7a980416844f2cd32591bed347fd1a2a
JBeni <jitca.beniamin@gmail.com>
2021-01-31 21:58:36 +0200

    sample net core web api with GraphQL implementation

---
+++ WebAPI_GraphQL/GraphQL/StarWarsQuery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GraphQL;
+using GraphQL.Types;
+using WebAPI_GraphQL.GraphQL.Types;
+
+namespace WebAPI_GraphQL.GraphQL
+{
+    public class StarWarsQuery : ObjectGraphType<object>
+    {
+        public StarWarsQuery(StarWarsData data)
+        {
+            Name = "Query";
+
+            Field<CharacterInterface>("hero", resolve: context => data.GetDroidByIdAsync("3"));
+            Field<HumanType>(
+                "human",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "id", Description = "id of the human" }
+                ),
+                resolve: context => data.GetHumanByIdAsync(context.GetArgument<string>("id"))
+            );
+        }
+    }
+}
---
+++ WebAPI_GraphQL/Startup.cs
@@ -0,0 +1,54 @@
+using GraphQL.Server;
+using GraphQL.Types;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using WebAPI_GraphQL;
+using WebAPI_GraphQL.GraphQL;
+using WebAPI_GraphQL.GraphQL.Types;
+
+namespace WebAPI_GraphQL
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<StarWarsData>();
+            services.AddSingleton<StarWarsQuery>();
+            services.AddSingleton<StarWarsMutation>();
+            services.AddSingleton<HumanType>();
+            services.AddSingleton<HumanInputType>();
+            services.AddSingleton<DroidType>();
+            services.AddSingleton<CharacterInterface>();
+            services.AddSingleton<EpisodeEnum>();
+            services.AddSingleton<ISchema, StarWarsSchema>();
+
+            services.AddLogging(builder => builder.AddConsole());
+            services.AddHttpContextAccessor();
+
+            services.AddGraphQL(options =>
+            {
+                options.EnableMetrics = true;
+            })
+            .AddErrorInfoProvider(opt => opt.ExposeExceptionStackTrace = true)
+            .AddSystemTextJson()
+            .AddUserContextBuilder(httpContext => new GraphQLUserContext { User = httpContext.User });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            // add http for Schema at default url /graphql
+            app.UseGraphQL<ISchema>();
+
+            // use graphql-playground at default url /ui/playground
+            app.UseGraphQLPlayground();
+        }
+    }
+}
